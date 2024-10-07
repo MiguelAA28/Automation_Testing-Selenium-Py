1ccc77dedd10e60ec8bdd12a71d160376474f625
Miguel A Arango <73609426+MiguelAA28@users.noreply.github.com>
2024-10-07 14:46:07 +0200

    Create README.TC Automation 2 - flow Citas 1 - Ons Web.py

---
+++ README.TC Automation 2 - flow Citas 1 - Ons Web.py
@@ -0,0 +1,16 @@
+﻿# Scripts_Automation_Testing-Vol-2
+
+# In this T.C we pretend validate the section "Citas" into the ONS web page. The main features about this section are the creation of an appointment on a certain date that can be visible to all users. At the end of the creation date, the sistem will issue a message "Tu anuncio para quedar se ha publicado con éxito.", validate the menssage successfully.
+
+# The code is divided into the folowing steeps:
+
+# 1. Import the necessary functions such as "webdriver", "by", "time" and "select"
+# 2. Get in into the URL page and adjust the window size.
+# 3. Verify the login view (user and password) and the initial pop up.
+# 4. Verify entry to the page and select the section "Citas"
+# 5. Get in into the option "publicar cita para quedar"
+# 6. Complete the dropdown options such as "poner anuncio", "y mostrarlo", "zonas", "ciudad", "que buscas" y "detalles".
+# 7. Select "publicar anuncio".
+# 8. Validate the messagem "Tu anuncio para quedar se ha publicado con éxito."
+
+The result of the execution showed 0 errors
