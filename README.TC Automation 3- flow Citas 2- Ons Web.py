13cc733a5140a819ac3f1b6f4623a7c567e725a3
Miguel A Arango <73609426+MiguelAA28@users.noreply.github.com>
2024-10-07 19:07:37 +0200

    Create README.TC Automation 3- flow Citas 2- Ons Web.py

---
+++ README.TC Automation 3- flow Citas 2- Ons Web.py
@@ -0,0 +1,15 @@
+﻿# Scripts_Automation_Testing-Vol-3
+
+# In this T.C we pretend validate the section "Citas" into the ONS web page. The main features about this section is the dating search using the own filters from the view (search by province and city).
+
+# The code is divided into the folowing steeps:
+
+# 1. Import the necessary functions such as "webdriver", "by", "time" and "select"
+# 2. Get in into the URL page and adjust the window size.
+# 3. Verify the login view (user and password) and the initial pop up.
+# 4. Verify entry to the page and select the section "Citas"
+# 5. Select the dropdown filters by province and city 
+# 6. Select the option "mostrar anuncios".
+# 7. Select any day for view the dates availables.
+
+# The result of the execution showed 0 errors
