2fbf4de5c448293d21341f0b5cc1e91a561a30ba
Miguel A Arango <73609426+MiguelAA28@users.noreply.github.com>
2024-10-16 20:11:50 +0200

    Create README.TC Automation 5 - flow Login - RSA Web.py

---
+++ README.TC Automation 5 - flow Login - RSA Web.py
@@ -0,0 +1,14 @@
+﻿# Scripts_Automation_Testing-Vol-3
+
+# In this T.C we pretend validate the  fail login message and their respective components from practice website of testing academy "Rahul Shetty Academy"
+
+# The code is divided into the folowing steeps:
+
+# 1. Import the necessary functions such as "webdriver", "by", "WebDriver Wait", "services", "time" and "Expected Conditions"
+# 2. Get in into the URL page and adjust the window size.
+# 3. Get In into the link in the top rigth in the screen and redirect to a new page for copy wrong user and password.
+# 3. Verify the login view (wrong user and password).
+# 4. Verify messagem for wrong user and password "letter in red" (assert method)
+
+
+# The result of the execution showed 0 errors
